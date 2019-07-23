826c94397c61c5b5fa90465667ee212d9bb992d3
Mikhael Vasilkovsky <mikhael.vasilkovsky@cognizant.com>
2019-07-23 16:15:23 -0500

    Circuit Breaker

--- components/allocations/src/main/java/io/pivotal/pal/tracker/allocations/ProjectClient.java
+++ components/allocations/src/main/java/io/pivotal/pal/tracker/allocations/ProjectClient.java
@@ -1,18 +1,31 @@
 package io.pivotal.pal.tracker.allocations;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.web.client.RestOperations;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class ProjectClient {
 
     private final RestOperations restOperations;
     private final String registrationServerEndpoint;
 
+    private Map<Long, ProjectInfo> cachedProjects = new ConcurrentHashMap<>();
+
     public ProjectClient(RestOperations restOperations, String registrationServerEndpoint) {
         this.restOperations= restOperations;
         this.registrationServerEndpoint = registrationServerEndpoint;
     }
 
+    @HystrixCommand(fallbackMethod = "getProjectFromCache")
     public ProjectInfo getProject(long projectId) {
-        return restOperations.getForObject(registrationServerEndpoint + "/projects/" + projectId, ProjectInfo.class);
+        cachedProjects.put(projectId, restOperations.getForObject(registrationServerEndpoint + "/projects/" + projectId, ProjectInfo.class));
+
+        return getProjectFromCache(projectId);
+    }
+
+    public ProjectInfo getProjectFromCache(long projectId) {
+        return cachedProjects.get(projectId);
     }
 }
